dc9e1f7fd25393ad900bf7e3e7541c0d813bae59
Ben Oakenfull <benoakenfull@gmail.com>
2020-04-10 16:38:09 +0100

    updates

--- src/component.stories.js
+++ src/component.stories.js
@@ -3,10 +3,17 @@ import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 
 import { Button } from 'kudu-core-framework'
+import { TextInput } from 'kudu-core-framework'
 
 storiesOf('components', module)
   .add('Button', () => (
     <Button>
       {text('Label', 'Hello Storybook')}
     </Button>
+  ))
+  .add('Text Input', () => (
+    <TextInput
+      onChange={action('change handler')}
+      placeholder={text('Placeholder', 'Placeholder')}
+    />
   ))
