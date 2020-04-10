3840c58da78e749c36266d0451f85b88973cf2c0
Ben Oakenfull <benoakenfull@gmail.com>
2020-04-10 16:42:01 +0100

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,8 @@ module.exports = {
     addons: [
       '@storybook/addon-knobs/register',
       '@storybook/addon-viewport/register',
-      '@storybook/addon-a11y/register'
+      '@storybook/addon-a11y/register',
+      '@storybook/addon-actions/register'
     ]
   };
   
--- src/component.stories.js
+++ src/component.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
 import { Button } from 'kudu-core-framework'
 import { TextInput } from 'kudu-core-framework'
