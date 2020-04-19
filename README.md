776b4dca3309263a32d8ed1bbc7da8dd9ac73152
Ben Oakenfull <benoakenfull@gmail.com>
2020-04-19 21:45:14 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kudu Core [Themework] Template
+
+This Design System has been bootstrapped with Themework.
--- rollup.config.js
+++ rollup.config.js
@@ -17,6 +17,7 @@ export default {
   ],
   plugins: [
     json(),
+    multi(),
     external(),
     url(),
     svgr(),
